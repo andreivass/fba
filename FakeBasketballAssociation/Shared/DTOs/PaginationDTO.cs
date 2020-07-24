10bc8ac837226a940214d000a619fcb45426f449
Andrei Vassiljev <avassiljev@bhsolutions.com>
2020-07-24 14:14:19 +0300

    latest

--- FakeBasketballAssociation/Shared/DTOs/PaginationDTO.cs
+++ FakeBasketballAssociation/Shared/DTOs/PaginationDTO.cs
@@ -7,6 +7,6 @@ namespace FakeBasketballAssociation.Shared.DTOs
     public class PaginationDTO
     {
         public int Page { get; set; } = 1;
-        public int RecordsPerPage { get; set; } = 10;
+        public int RecordsPerPage { get; set; } = 5;
     }
 }
